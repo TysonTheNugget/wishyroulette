88da3af4d680be7b18b34c432aca65a0556bde5b
TysonTheNugget <pokebitmap@gmail.com>
2025-08-10 23:52:06 -0400

    testwwwdw

--- api/index.js
+++ api/index.js
@@ -1,10 +1,3 @@
 // api/index.js
 const app = require('../backend');
-
-// Normalize the path so Express sees "/init", "/poll", etc.
-module.exports = (req, res) => {
-  if (req.url.startsWith('/api/index')) {
-    req.url = req.url.replace('/api/index', '');
-  }
-  return app(req, res);
-};
+module.exports = (req, res) => app(req, res);
